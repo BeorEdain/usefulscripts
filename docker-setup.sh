d2c0e026c8aaf8fa1046e7bf1f1557f3c68f8d05
Beor Edain <edain.beor@gmail.com>
2022-04-17 11:53:41 -0600

    Add install-vscodium.sh and update README.md to have proper formatting.

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,13 +1,33 @@
-#! /bin/sh -e
+#! /bin/bash -e
 
-# Install Docker Engine
-sudo apt remove docker docker-engine docker.io containerd runc
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+# GLOBAL
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+
+operating_system=$(grep '^NAME' /etc/os-release)
+
+# UBUNTU
+if [[ $operating_system == 'NAME="Ubuntu"' ]];
+then
+    echo "Operating system is Ubuntu"
+    sudo apt-get install -y ca-certificates curl gnupg lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# LINUX MINT
+elif [[ $operating_system == 'NAME="Linux Mint"' ]];
+then
+    echo "Operating system is Linux Mint"
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+fi
+
+# GLOBAL
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
