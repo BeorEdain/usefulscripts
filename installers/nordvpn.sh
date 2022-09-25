7fb4ec1d1d64e5433c34561c7d92828651057916
Beor Edain <edain.beor@gmail.com>
2022-09-25 07:34:05 -0700

    Modify nordvpn.sh to simplify install.

--- installers/nordvpn.sh
+++ installers/nordvpn.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
 if command -v curl &> /dev/null; then
-    echo "Downloading NordVPN installer with curl"
-    sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
-
-elif command -v wget &> /dev/null; then
-    echo "Downloading NordVPN installer with wget"
-     sh <(wget -qO https://downloads.nordcdn.com/apps/linux/install.sh)
+    echo "Downloading NordVPN installer..."
 
 else
-    echo "Neither curl nor wget exists on this system. Please install either one and rerun this script."
+	echo "curl is not installed, installing now..."
+	sudo apt install -y curl
+	echo "Installing NordVPN..."
     
 fi
 
-nordvpn connect
+sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+
+sudo usermod -aG nordvpn $USER
+
+echo "Please reboot your system to allow $USER to use NordVPN without sudo powers."
